9451871c9ab93a03a123a4e9d9a640c738c42c34
Maria (Malu) Luiza <148253498+marialuiza15@users.noreply.github.com>
2024-12-04 23:28:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ import re
 from collections import defaultdict
 import streamlit as st
 
-# Se necessário, forneça o caminho para o executável do tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-# Função para extrair texto de um PDF
+# Função para extrair texto do PDF
 def extrair_texto_pdf(pdf_path):
     doc = fitz.open(pdf_path)
     texto_extraido = ""
@@ -19,26 +18,22 @@ def extrair_texto_pdf(pdf_path):
     
     return texto_extraido
 
-# Função para processar e organizar informações da nota fiscal
+
 def extrair_informacoes_nota(texto):
     informacoes = defaultdict(str)
 
-    # Regex para informações específicas
-    cnpj_regex = r"(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})" #Funcionando
-    cpf_regex = r"(\d{3}\.\d{3}\.\d{3}-\d{2})" #Funcionando
+    cnpj_regex = r"(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})" 
+    cpf_regex = r"(\d{3}\.\d{3}\.\d{3}-\d{2})" 
     empresa_regex =  r"DE\s+(.*?)\s+OS" 
-    chave_acesso_regex = r"(\d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4})" #Funcionando
-    data_emissao_regex = r"(\d{2}/\d{2}/\d{4})" #Funcionando
+    chave_acesso_regex = r"(\d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{4})" 
+    data_emissao_regex = r"(\d{2}/\d{2}/\d{4})" 
 
-
-    # Extraindo informações com regex
     cnpjs = re.search(cnpj_regex, texto)
     cpf = re.search(cpf_regex, texto)
     empresa = re.search(empresa_regex, texto)
     chave_acesso = re.search(chave_acesso_regex, texto)
     data_emissao = re.search(data_emissao_regex, texto)
 
-    
     informacoes["Empresa"] = empresa.group(1)
     informacoes["CNPJs"] = cnpjs.group(1) if cnpjs else ""
     informacoes["CPF"] = cpf.group(1) if cpf else ""
@@ -50,19 +45,15 @@ def extrair_informacoes_nota(texto):
 st.title('Extração de Informações de Nota Fiscal')
 uploaded_file = st.file_uploader("Escolha um arquivo PDF", type="pdf")
 if uploaded_file is not None:
-    # Salvar o arquivo carregado para ser processado
+
     with open("uploaded_file.pdf", "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    # Extrair texto do PDF
     texto_pdf = extrair_texto_pdf("uploaded_file.pdf")
-
-    # Extrair informações da nota fiscal
     informacoes = extrair_informacoes_nota(texto_pdf)
 
-    # Exibir as informações extraídas
     st.subheader('Informações Extraídas:')
-    # Exibir informações extraídas
+
     st.write("Informacoes extraidas da Nota Fiscal:")
     st.write(f"CNPJs: {informacoes['CNPJs']}")
     st.write(f"CPF comprador: {informacoes['CPF']}")
